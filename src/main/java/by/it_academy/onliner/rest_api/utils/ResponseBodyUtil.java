8a7391fef230cb0d7eb87c80e46c16c7d62f99ab
iryna_s <emily_89@mail.ru>
2022-07-29 01:05:52 +0300

    feat: model added

---
+++ src/main/java/by/it_academy/onliner/rest_api/utils/ResponseBodyUtil.java
@@ -0,0 +1,25 @@
+package by.it_academy.onliner.rest_api.utils;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ResponseBody;
+
+import java.util.List;
+
+public class ResponseBodyUtil {
+
+    public static <T> T GetObjectByJsonPath(ResponseBody responseBody,
+                                                  String jsonPath, Class<T> type) {
+        return responseBody.jsonPath().getObject(jsonPath, type);
+    }
+
+    public static <T> List<T> GetObjectsByJsonPath(ResponseBody responseBody,
+                                                  String jsonPath, Class<T> type) {
+        return responseBody.jsonPath().getList(jsonPath, type);
+    }
+
+    public static String getStringJsonValue(ResponseBody responseBody, String jsonPath)
+    {
+        return JsonPath.from(responseBody.asString()).getString(jsonPath);
+    }
+
+}
